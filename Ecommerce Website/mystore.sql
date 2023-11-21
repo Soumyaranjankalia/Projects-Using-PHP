153c516c98813f92d2440b9d7bb229675af415f8
Soumyaranjankalia <a2soumya.ranjan@gmail.com>
2023-11-22 01:21:53 +0530

    added

--- Ecommerce Website/mystore.sql
+++ Ecommerce Website/mystore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2023 at 04:50 PM
+-- Generation Time: Nov 21, 2023 at 08:50 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -66,6 +66,36 @@ INSERT INTO `categories` (`category_id`, `category_title`) VALUES
 (5, 'Vegitables'),
 (6, 'Chips');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_title` varchar(100) NOT NULL,
+  `product_description` varchar(255) NOT NULL,
+  `product_keywords` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `product_image1` varchar(255) NOT NULL,
+  `product_image2` varchar(255) NOT NULL,
+  `product_image3` varchar(255) NOT NULL,
+  `product_price` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_title`, `product_description`, `product_keywords`, `category_id`, `brand_id`, `product_image1`, `product_image2`, `product_image3`, `product_price`, `date`, `status`) VALUES
+(1, 'Fresh Mangoes', 'Mangoes are always testy', 'mango, green mango, zomato, fresh', 1, 2, 'mango.jpg', 'mango2.jpg', 'mango3.jpg', '200', '2023-11-21 19:41:30', 'true'),
+(2, 'Fresh Mangoes', 'Mangoes are always testy', 'mango, green mango, zomato, fresh', 1, 2, 'mango.jpg', 'mango2.jpg', 'mango3.jpg', '200', '2023-11-21 19:43:21', 'true'),
+(3, 'Fresh Mangoes', 'Mangoes are always testy', 'mango, green mango, zomato, fresh', 1, 2, 'mango.jpg', 'mango2.jpg', 'mango3.jpg', '200', '2023-11-21 19:45:26', 'true');
+
 --
 -- Indexes for dumped tables
 --
@@ -83,6 +113,12 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -97,6 +133,12 @@ ALTER TABLE `brands`
 --
 ALTER TABLE `categories`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
